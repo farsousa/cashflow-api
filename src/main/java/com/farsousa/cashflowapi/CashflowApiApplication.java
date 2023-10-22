32e81e3cf3d04d21a32d5d6338073342888b37d8
farsousa <andersoncaj26@gmail.com>
2023-10-22 09:21:02 -0300

    estrutura inicial

---
+++ src/main/java/com/farsousa/cashflowapi/CashflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.farsousa.cashflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farsousa/cashflowapi/CashflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farsousa.cashflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
